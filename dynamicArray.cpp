71dffdc1c601cb7ecd7ac6ef9e6f396931681100
vincent55147 <vincent7418529630@gmail.com>
2017-11-28 19:30:06 +0800

    Update dynamicArray.cpp

--- dynamicArray.cpp
+++ dynamicArray.cpp
@@ -38,7 +38,7 @@ string* create2DArray(int rows,int colmns) {
 	return head;
 }
 string* addEntry(string* dynamicArray,int size, string newEntry1,string newEntry2) {
-	if(size == usernum) { //©ñ¤j­«¶} 
+	if(size == usernum) { //æ”¾å¤§å¾Œç¸®å° 
 		usernum *= 2;
 		string* head = new string [usernum*2];
 		for(int i=0;i<size-1;i++){
